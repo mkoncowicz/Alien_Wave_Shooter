dd4384e4009b9343bd58b628a5e33012fab647b4
unknown <mkoncowicz@gmail.com>
2024-05-14 21:52:51 +0200

    Enemy logic

--- enemy/enemyAlien.gd
+++ enemy/enemyAlien.gd
@@ -1,27 +1,18 @@
 extends Entity
 
-var player_chase = false
-var player = null
+@onready var nav_agent = $NavigationAgent2D
 
 func _physics_process(delta):
-	if player_chase:
-		position += (player.position - position)/speed
-		
-		$AnimatedSprite2D.play("run")
-		
-		if(player.position.x - position.x) < 0:
-			$AnimatedSprite2D.flip_h = true
-		else:
-			$AnimatedSprite2D.flip_h = false
-	else:
-		$AnimatedSprite2D.play("idle")
+	var current_location = global_transform.origin
+	var next_location = nav_agent.get_next_path_position()
+	var new_velocity = (next_location - current_location).normalized() * 100
+	
+	velocity = new_velocity
+	move_and_slide()
 
-func _on_detect_radius_body_entered(body):
-	player = body
-	player_chase = true
+func update_target_location(target_location):
+	nav_agent.target_position = target_location
 
-
-func _on_detect_radius_body_exited(body):
-	player = null
-	player_chase = false
-
+func _on_navigation_agent_2d_target_reached():
+	#attack here
+	print("attack")
