2373a30d65ffcc177ff4f1f146a09894da6e9754
unknown <mkoncowicz@gmail.com>
2024-05-20 21:26:36 +0200

    enemy animations

--- enemy/enemySlime.gd
+++ enemy/enemySlime.gd
@@ -3,12 +3,19 @@ extends Entity
 @onready var nav_agent = $NavigationAgent2D
 
 func _physics_process(delta):
+	animated_sprite.play("move")
+	
 	var current_location = global_transform.origin
 	var next_location = nav_agent.get_next_path_position()
 	var new_velocity = (next_location - current_location).normalized() * 60
 	
 	velocity = new_velocity
 	move_and_slide()
+	
+	if nav_agent.target_position.x < global_position.x:
+		animated_sprite.flip_h = true
+	else:
+		animated_sprite.flip_h = false
 
 func update_target_location(target_location):
 	nav_agent.target_position = target_location
