4fb2a2a46b97c7fc55342a844ac9004a6bfd56e5
Jacek Falkowski <jf300388@student.polsl.pl>
2024-05-27 19:55:44 +0200

    Bullet destroy light effect

--- Bullet_hit_animation.gd
+++ Bullet_hit_animation.gd
@@ -3,12 +3,16 @@ extends AnimatedSprite2D
 var weapon_type : String 
 func _ready():
 	if weapon_type == "pistol":
+		$PointLight2D.set_color(Color(0.9765, 1, 0, 1))
 		play("Pistol")
 	if weapon_type == "machinegun":
+		$PointLight2D.set_color(Color(1, 1, 0, 1))
 		play("Machinegun")
 	if weapon_type == "shotgun":
+		$PointLight2D.set_color(Color(1, 0.0706, 0.0706, 1))
 		play("Shotgun")
 	if weapon_type == "railgun":
+		$PointLight2D.set_color(Color(0, 1, 1, 1))
 		play("Railgun")
 
 	
